c0f16c5db1d57ed006521a3f856fd892f1b8f561
Duncan Stuart <dgmstuart@gmail.com>
2013-11-16 21:36:26 +0000

    Pulling back a whole debate from a url

--- lib/debates.rb
+++ lib/debates.rb
@@ -2,28 +2,42 @@ require 'twfy'
 
 class Debates
   # Example:
-  # Debates.new.debates_from_url(:commons, "http://www.publications.parliament.uk/pa/cm201314/cmhansrd/cm130521/debtext/130521-0002.htm")
+  # Debates.new.debate_from_url(:lords, "http://www.publications.parliament.uk/pa/ld201314/ldhansrd/text/130624-0001.htm#13062413000429")
 
   def client
     @client ||= Twfy::Client.new(Figaro.env.theyworkforyou_api_key)
   end
 
   def debate_from_url(type, url)
-    raise %{Type must be either "commons" or "lords"} unless ["commons", "lords"]
+    raise %{Type must be either :commons or :lords} unless [:commons, :lords].include?(type)
 
     url = client.convert_url(url: url)
     gid = url.id
 
-    client.debate(type: type.to_s, gid: gid)
-    # API Definition:
-    # getDebates: {
-    #   require: :type,
-    #   require_one_of: [:date, :person, :search, :gid],
-    #   allow_dependencies: {
-    #     search: [:order, :page, :num],
-    #     person: [:order, :page, :num]
-    #   }
-    # }
+    # Until we work out how to do this properly, assume it's just
+    #  'replace the last digit chunks with a 2'
+    whole_debate_gid = gid.sub(/\.\d+$/,".2")
+
+    begin
+      client.debates(type: type.to_s, gid: whole_debate_gid)
+      # API Definition:
+      # getDebates: {
+      #   require: :type,
+      #   require_one_of: [:date, :person, :search, :gid],
+      #   allow_dependencies: {
+      #     search: [:order, :page, :num],
+      #     person: [:order, :page, :num]
+      #   }
+      # }
+    rescue OpenURI::HTTPError => e
+      raise %{OpenURI::HTTPError #{e.message}
+
+You tried to get the following debate:
+-- gid:  #{whole_debate_gid}
+-- type: #{type.to_s}
+
+}
+    end
   end
 
   private
