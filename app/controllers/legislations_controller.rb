9869fa3fb67b0f1ba6458957bcd99a200124ac06
Philip Vieira <philip@vallin.se>
2013-11-16 21:20:46 +0000

    Query for legislations in Legislation controller.

--- app/controllers/legislations_controller.rb
+++ app/controllers/legislations_controller.rb
@@ -10,10 +10,12 @@ class LegislationsController < ApplicationController
   include ActionView::Helpers::TextHelper
 
   def index
+    @legislations = Legislation.all
   	@mdoc = simple_format(getLegislationParsedForTitle)
   end
 
   def show
+    @legislation = Legislation.find(params[:id])
   end
 
 end
