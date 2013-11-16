4d15422bf9bd6fb112d5c6947e375a15008fb4e6
Philip Vieira <philip@vallin.se>
2013-11-16 17:01:44 +0000

    Add Legislation controller/view base.

---
+++ app/controllers/legislations_controller.rb
@@ -0,0 +1,9 @@
+class LegislationsController < ApplicationController
+
+  def index
+  end
+
+  def show
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,6 @@
 Rsparly2013::Application.routes.draw do
+
+  resources :legislation, path: '/', only: [:index, :show] do
+  end
+
 end
