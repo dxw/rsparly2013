1244b6de5fee45ea065e25bed8c52c09279ad337
Philip Vieira <philip@vallin.se>
2013-11-16 16:47:12 +0000

    Clean up and configure some boilerplate.

--- config/application.rb
+++ config/application.rb
@@ -1,28 +1,25 @@
 require File.expand_path('../boot', __FILE__)
 
-# Pick the frameworks you want:
 require "active_record/railtie"
 require "action_controller/railtie"
 require "action_mailer/railtie"
 require "sprockets/railtie"
-# require "rails/test_unit/railtie"
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env)
 
 module Rsparly2013
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
 
-    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
+    config.time_zone = 'UTC'
+    config.i18n.default_locale = :en
 
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    config.generators do |g|
+      g.orm :active_record
+      g.test_framework nil
+      g.stylesheet_engine :sass
+      g.template_engine :haml
+      g.template_engine :coffee
+    end
+
   end
 end
