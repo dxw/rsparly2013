8dc683edd643e9cc23a0dbdc85730a9254e898d5
Duncan Stuart <dgmstuart@gmail.com>
2013-11-17 02:05:32 +0000

    producing an array of debate segments about individual amendments

--- lib/debates.rb
+++ lib/debates.rb
@@ -2,24 +2,68 @@ require 'twfy'
 
 class Debates
   # Example:
+
+  #Simple (replace last digit with 2)
   # Debates.new.debate_from_url(:lords, "http://www.publications.parliament.uk/pa/ld201314/ldhansrd/text/130624-0001.htm#13062413000429")
+  # Debates.new.amendment_debates_from_url(:lords, "http://www.publications.parliament.uk/pa/ld201314/ldhansrd/text/130624-0001.htm#13062413000429")
+
+  # List (select relevant thing from list)
+  # Debates.new.debate_from_url(:commons, "http://www.publications.parliament.uk/pa/cm201314/cmhansrd/cm130520/debtext/130520-0002.htm#13052013000002")
+
+  #
+  # Debates.new.debate_from_url(:commons, "http://www.publications.parliament.uk/pa/cm201314/cmhansrd/cm130521/debtext/130521-0002.htm#13052156000001")
+
 
   def client
-    @client ||= Twfy::Client.new(Figaro.env.theyworkforyou_api_key)
+    # @client ||= Twfy::Client.new(Figaro.env.theyworkforyou_api_key)
+    Twfy::Client.new(Figaro.env.theyworkforyou_api_key)
+  end
+
+  def amendments_from_debate(debate_array)
+    debate_array.map{|a| a.body.match(/<b>(\d+[A-Z]*):<\/b>+/)}.compact.map{|x| x[1]}
+  end
+
+  def amendments_from_debate_with_indexes(debate_hash)
+    foo = []
+    # debate_array.map{|a| a.body}.each_with_index{|body, i| m = body.match(/<b>(\d+[A-Z]*):<\/b>+/);  foo[match[1]] = i if match }.compact
+    debate_hash.map{|a| a.body}.each_with_index{|body, i| m = body.match(/<b>(\d+[A-Z]*):<\/b>+/);  foo << [m[1], i] if m }.compact
+
+    return foo
+    # OUTPUT = e.g.
+  end
+
+  def amendment_debate(amendment, debate_array, amendments_from_debate_with_indexes)
+    start_index =nil
+    end_index = nil
+
+    amendments_from_debate_with_indexes.each_with_index do |amendment_and_index, n|
+      if amendment == amendment_and_index[0]
+        @start_index = amendment_and_index[1]
+        @end_index = 10000000000 # can't be bothered to calculate the actual length
+        next_element = amendments_from_debate_with_indexes[n+1]
+
+        @end_index = next_element[1] unless next_element.nil?
+      end
+    end
+
+    raise "Start index is nil!" if @start_index.nil?
+    raise "End index is nil!" if   @end_index.nil?
+    debate_array[@start_index..@end_index]
+  end
+
+  def amendment_debates_from_url(type, url)
+    debate_array = debate_from_url(type, url)
+    afdwi = amendments_from_debate_with_indexes(debate_array)
+    afdwi.collect {|amendment, i| amendment_debate(amendment, debate_array, afdwi)}
   end
 
   def debate_from_url(type, url)
     raise %{Type must be either :commons or :lords} unless [:commons, :lords].include?(type)
 
-    url = client.convert_url(url: url)
-    gid = url.id
-
-    # Until we work out how to do this properly, assume it's just
-    #  'replace the last digit chunks with a 2'
-    whole_debate_gid = gid.sub(/\.\d+$/,".2")
+    gid = gid_from_url(type, url)
 
     begin
-      client.debates(type: type.to_s, gid: whole_debate_gid)
+      client.debates(type: type.to_s, gid: gid)
       # API Definition:
       # getDebates: {
       #   require: :type,
@@ -33,12 +77,45 @@ class Debates
       raise %{OpenURI::HTTPError #{e.message}
 
 You tried to get the following debate:
--- gid:  #{whole_debate_gid}
+-- gid:  #{gid}
 -- type: #{type.to_s}
 
 }
     end
   end
+
+  def gid_from_url(type, url)
+    # converted_url = client.convert_url(url: url)
+    # interim_gid = converted_url.id
+
+    interim_gid = "2013-06-24a.507.2"
+
+    # CASE 1 - simple
+    if false
+      # WHAT'S THE CONDITION HERE????
+
+
+      # Until we work out how to do this properly, assume it's just
+      #  'replace the last digit chunks with a 2'
+      gid = interim_gid.sub(/\.\d+$/,".2") if convert
+
+    # CASE 2 - Multiple bills on the page
+    elsif false
+      # WHAT'S THE CONDITION HERE????
+      gid = client.debates(type: type.to_s, gid: interim_gid)
+      # In this case, delete anything which isn't nil or a big number
+    else
+      gid = interim_gid
+    end
+
+    gid
+
+  end
+
+
+  # CASE1:
+  # Content count + a load of nils = trash anything before it
+
 
   private
 
